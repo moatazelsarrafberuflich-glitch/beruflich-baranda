726cfb8640c6770f6a428130c3bb48944a363b33
moatazelsarrafberuflich-glitch <moatazelsarrafberuflich@gmail.com>
2026-09-05 08:42:19 +0000

    deleting

--- diarino-app/app/admin/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from "expo-router";
-
-export default function AdminLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false, presentation: 'modal' }}>
-      <Stack.Screen name="index" />
-    </Stack>
-  );
-}
